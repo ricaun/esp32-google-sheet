989e61df3f95e36ca83f63f951d9c499bd0a95e0
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-03-01 20:42:13 -0300

    dht11

---
+++ esp32-google-sheet-hx711-dht11/dht11.ino
@@ -0,0 +1,28 @@
+#include <dht.h>
+
+#define DHT11_PIN 7
+
+dht dht11;
+
+void dht_begin()
+{
+
+}
+
+float dht_read_temperature()
+{
+    int chk = dht11.read11(DHT11_PIN);
+    float f = dht11.temperature;
+    Serial.print("dht11 temperature: ");
+    Serial.println(f);
+    return f;
+}
+
+float dht_read_humidity()
+{
+    int chk = dht11.read11(DHT11_PIN);
+    float f = dht11.humidity;
+    Serial.print("dht11 humidity: ");
+    Serial.println(f);
+    return f;
+}
